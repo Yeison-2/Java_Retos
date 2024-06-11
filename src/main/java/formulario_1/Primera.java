a48ea5b31cf8b45b830e987b8179194f4a690772
YeisonRS <yeisonstivenromero@gmail.com>
2024-06-10 21:24:44 -0500

    Ejercicio de formulario

---
+++ src/main/java/formulario_1/Primera.java
@@ -0,0 +1,30 @@
+/**
+ * Docente: Magda Pineda
+ * Universidad juan de castellanos
+ * Asignatura: POO
+ * Descripción: 
+ * Fecha: //
+ * Autor: Yeison Stiven Romero Salinas
+ */
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package formulario_1;
+
+/**
+ *
+ * @author Yeison Romero Salinas
+ */
+
+import formulario_1.*;
+
+public class Primera {
+    public static void main(String[] args) {
+        Vista_Formulario vista_Formulario = new Vista_Formulario();
+        vista_Formulario.setVisible(true);
+    }
+
+}
